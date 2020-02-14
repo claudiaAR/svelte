20f507fa1a5ae6dc21879b7a7ca616841fe7047e
Claudia Aranda <claudia_15_@hotmail.com>
2020-02-14 13:23:00 +0100

    svelte click-event

--- svete-playground/src/App.svelte
+++ svete-playground/src/App.svelte
@@ -1,11 +1,24 @@
 
 <script>
-	export let name; 
+	import { onMount } from 'svelte';
+	
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={handleClick}>
+		Click me
+	</button>
+
+	<h1>
+		Clicked {count} {count === 1 ? 'time' : 'times'}
+	</h1>
 </main>
 
 <style>
@@ -14,19 +27,23 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-		background: green;
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #18a4e0;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	button{
+		background: #222d6d;
+		color: #f8b4c1;
+		border: none;
+		padding: 2rem;
+		font-size: 1.5rem; 
+		text-transform: uppercase;
 	}
+
+	
 </style>
